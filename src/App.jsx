73a29b7dfcf168cbe172d2dcc858ae9e3055fcbd
Prayush Dave <prayushdave@Prayushs-MacBook-Air.local>
2024-01-06 23:36:13 +0530

    Remove useState Bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 
 import './App.css'
 import NoteCanvas from './components/NoteCanvas'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='main'>
